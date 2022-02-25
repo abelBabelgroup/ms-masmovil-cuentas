2562d04d9a774ee76871fadb17047795963a23ae
karla.villamediana@babel.es <Fg.Kv080910Fg.Kv080910Fg.Kv080910>
2022-02-25 13:31:35 +0100

    creación entidad Contacto

--- src/main/java/com/masmovil/repository/model/Contacto.java
+++ src/main/java/com/masmovil/repository/model/Contacto.java
@@ -1,4 +1,15 @@
 package com.masmovil.repository.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Contacto")
 public class Contacto {
+
 }
