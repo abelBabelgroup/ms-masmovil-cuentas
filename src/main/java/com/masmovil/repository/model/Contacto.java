8e0475ad607bcd7aec6c4c855f984756b3b0b085
karla.villamediana@babel.es <Fg.Kv080910Fg.Kv080910Fg.Kv080910>
2022-02-25 14:21:03 +0100

    creación entidad Contacto y Oferta

--- src/main/java/com/masmovil/repository/model/Contacto.java
+++ src/main/java/com/masmovil/repository/model/Contacto.java
@@ -1,15 +1,53 @@
 package com.masmovil.repository.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Getter
-@Setter
-@Table(name = "Contacto")
-public class Contacto {
+@Data
+@NoArgsConstructor
+@Table(name = "contacto")
+public class Contacto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_CONTACTO")
+    private Long idContacto;
+
+    @Column(name = "ID_CUENTA")
+    private Integer idCuenta;
+
+    @Column(name = "TIPO_DOCUMENTO")
+    private String tipoDocumento;
+
+    @Column(name = "DOCUMENTO")
+    private String documento;
+
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Column(name = "APELLIDOS")
+    private String apellidos;
+
+    @Column(name = "TELEFONO")
+    private String telefono;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "TIPO")
+    private String tipo;
+
+    @Column(name = "APODERADO")
+    private boolean apoderado;
 
 }
--- src/main/java/com/masmovil/repository/model/Oferta.java
+++ src/main/java/com/masmovil/repository/model/Oferta.java
@@ -1,4 +1,50 @@
 package com.masmovil.repository.model;
 
-public class Oferta {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "oferta")
+public class Oferta implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_OFERTA")
+    private Long idOferta;
+
+    @Column(name = "ID_OFERTA_ORIGEN")
+    private Integer IdOfertaOrigen;
+
+    @Column(name = "ID_CUENTA")
+    private String idCuenta;
+
+    @Column(name = "ID_PROPIETARIO")
+    private String idPropietario;
+
+    @Column(name = "VERSION")
+    private String version;
+
+    @Column(name = "FEC_ALTA")
+    private String fechaAlta;
+
+    @Column(name = "FEC_ESTADO")
+    private String fechaEstado;
+
+    @Column(name = "ESTADO")
+    private String estado;
+
+    @Column(name = "CONTENIDO")
+    private String contenido;
+
 }
