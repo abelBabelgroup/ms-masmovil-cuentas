3d29eabdf5de276e77eda8ebc86611c5befbbbd7
Abel Antúnez <abel.ronin@gmail.com>
2022-02-28 15:54:57 +0100

    Mapper movido a la capa de servicio

--- src/main/java/com/masmovil/service/CuentaService.java
+++ src/main/java/com/masmovil/service/CuentaService.java
@@ -1,15 +1,44 @@
-package com.masmovil.service;
+package com.masmovil.service.impl;
 
+import com.masmovil.repository.CuentaRepository;
 import com.masmovil.repository.model.Cuenta;
+import com.masmovil.service.CuentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public interface CuentaService {
+@Service
+public class CuentaServiceImpl implements CuentaService {
 
-    List<Cuenta> getAccounts(List<String> ownerId);
-    List<Cuenta> getAccounts(List<String> ownerId, String name);
-    List<Cuenta> getAccounts(List<String> ownerId, String name, String cif);
+    @Autowired
+    private CuentaRepository cuentaRepository;
 
-    Optional<Cuenta> getAccountInfo(Long accountId);
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cuenta> getAccounts(List<String> ownerId) {
+        List<Cuenta> cuentas = new ArrayList<>();
+        ownerId.forEach(id -> cuentas.addAll(cuentaRepository.findByIdPropietario(id)));
+
+        return cuentas;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cuenta> getAccounts(List<String> ownerId, String name, String cif) {
+        List<Cuenta> cuentas = new ArrayList<>();
+        //ownerId.forEach(id -> cuentas.addAll(cuentaRepository.findByIdPropietarioAndNombreOrCif(id, name, cif)));
+        ownerId.forEach(id -> cuentas.addAll(cuentaRepository.findByIdPropietarioEqualsAndNombreEqualsOrCifEquals(id, name, cif)));
+
+        return cuentas;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Cuenta> getAccountInfo(Long accountId) {
+        return cuentaRepository.findById(accountId);
+    }
 }
