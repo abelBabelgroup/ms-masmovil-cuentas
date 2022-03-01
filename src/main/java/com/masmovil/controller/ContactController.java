fc612cc005f95d0860ac1e7d3312061725482241
karla.villamediana@babel.es <Fg.Kv080910Fg.Kv080910Fg.Kv080910>
2022-03-01 16:08:26 +0100

    creación de los métodos update y delete del contacto

--- src/main/java/com/masmovil/controller/ContactController.java
+++ src/main/java/com/masmovil/controller/ContactController.java
@@ -1,13 +1,16 @@
 package com.masmovil.controller;
 
 import com.masmovil.domain.dto.ContactDto;
-import com.masmovil.repository.entity.Contact;
 import com.masmovil.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +21,6 @@ import javax.validation.Valid;
 @RequestMapping("/contact")
 public class ContactController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ContactController.class);
-
     @Autowired
     private ContactService contactService;
 
@@ -29,15 +30,16 @@ public class ContactController {
         return ResponseEntity.ok(contactDto1);
     }
 
-//    @PutMapping("accounts/{accountId}/contact")
-//    public ResponseEntity<ContactDto> updateContact(@RequestParam Long accountId) {
-//        contactService.updateContact(accountId);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("accounts/{accountId}/contact")
-//    public ResponseEntity<Void> deleteContact(@RequestParam Long accountId) {
-//        contactService.deleteContact(accountId);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
+    @PutMapping("/{contactId}")
+    public ResponseEntity<Void> updateContact(@PathVariable Long contactId,
+        @RequestBody ContactDto contactDto) {
+        contactService.updateContact(contactId, contactDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{contactId}")
+    public ResponseEntity<Void> deleteContact(@PathVariable Long contactId) {
+        contactService.deleteContact(contactId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
